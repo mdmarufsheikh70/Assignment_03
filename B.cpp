fd391957f42e759ac3ffc913545c32c5d0caa61a
Maruf <mdmarufsheikh707@gmail.com>
2025-07-04 01:08:47 +0600

    Add files via upload

---
+++ B.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
+
+int main()
+{
+    optimize();
+
+    int n; 
+    cin >> n;
+
+    vector<int> A(n);
+    vector<int> B(n);
+    vector<int> C(n);
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> A[i] >> B[i];
+        if(B[i] > A[i])
+        {
+            C[i] = 0;
+        }
+        else
+        {
+            C[i] = A[i] - B[i];
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << C[i] << endl;
+    }
+    return 0;
+}
