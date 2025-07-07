8403c65e26e9db55bbb5bf1199313a33ce9758ef
Maruf <mdmarufsheikh707@gmail.com>
2025-07-07 15:57:22 +0600

    Add files via upload

---
+++ H.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main()
+{
+    optimize();
+
+    int n;
+    cin >> n;
+
+    vector<int> A(n);
+    
+    for(int i = 0; i < n; i++)
+    {
+        cin >> A[i];
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        if(A[i] % 3 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
